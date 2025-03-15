14fcd1661bdab0418e21b22b4632b377ad7bbbdb
Cashyn Murray <cashyn@startuphakk.com>
2025-03-15 12:05:59 -0700

    Temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
 
+    menuToggle.addEventListener("click", function() {
+        if (menu.style.display === "block") {
+            menu.style.display = "none";
+            menuToggle.innerHTML = "&#9776;"; 
+        } else {
+            menu.style.display = "block";
+            menuToggle.innerHTML = "&#10006;"; 
+        }
+    });
+
+    document.getElementById("year").textContent = new Date().getFullYear();
+    document.getElementById("lastModified").textContent = document.lastModified;
+});
